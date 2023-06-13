9d13eedbe28ff904134aa4bf68e0e5d6e4f1ffdf
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-06-13 16:02:32 +0200

    feature: add the limit command.
    * This command allowed player to enable the limiter with a specific speed value.

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -24,3 +24,25 @@ CreateThread(function()
         end
     end
 end)
+
+RegisterCommand('limit', function(source, args)
+    local playerPed = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+    if (IsPedInAnyVehicle(playerPed, false) and GetPedInVehicleSeat(vehicle, -1) == playerPed) then
+        if (args[1] ~= nil) then
+            local speed = tonumber(args[1])
+            if (speed ~= nil) then
+                speedLimit = speed / 3.6
+                SetEntityMaxSpeed(vehicle, speedLimit)
+                TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Limitation de vitesse activée à " .. speed .. " km/h"}})
+            else
+                TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Vitesse invalide"}})
+            end
+        else
+            limitSpeed = false
+            SetEntityMaxSpeed(vehicle, 999.0)
+            TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Limitation de vitesse désactivée"}})
+        end
+    end
+end)
