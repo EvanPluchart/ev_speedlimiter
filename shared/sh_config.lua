945a353b58bae315887adf488d795360e651b18f
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-06-13 16:25:40 +0200

    feature: add configs.

---
+++ shared/sh_config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.controlKey = 38 -- Key for enable/desable speedlimit (https://docs.fivem.net/docs/game-references/controls/)
+Config.speedUnit = "kph" -- Speed unit (kph or mph)
+Config.invalidSpeed = "^1[Limitateur de vitesse]^7 Vitesse invalide" -- Message when speed is invalid
+Config.speedLimitEnabled = "^1[Limitateur de vitesse]^7 Limitation de vitesse activée à " -- Message when speedlimit is enabled
+Config.speedLimitDisabled = "^1[Limitateur de vitesse]^7 Limitation de vitesse désactivée" -- Message when speedlimit is disabled
