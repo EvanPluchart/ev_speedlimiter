7eaced680c0163cda376d88da8d68dec131220e7
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-06-13 16:22:20 +0200

    feature: add config.

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,6 +1,15 @@
 local limitSpeed = false
 local speedLimit = 0.0
-local controlKey = 38
+local speedRatio
+local speedUnit
+
+if (Config.speedUnit == "kph") then
+    speedRatio = 3.6
+    speedUnit = "km/h"
+else
+    speedRatio = 2.236936
+    speedUnit = "mph"
+end
 
 CreateThread(function()
     while (true) do
@@ -10,15 +19,15 @@ CreateThread(function()
         local vehicle = GetVehiclePedIsIn(playerPed, false)
 
         if (IsPedInAnyVehicle(playerPed, false) and GetPedInVehicleSeat(vehicle, -1) == playerPed) then
-            if (IsControlJustPressed(0, controlKey)) then
+            if (IsControlJustPressed(0, Config.controlKey)) then
                 limitSpeed = not limitSpeed
                 if (limitSpeed) then
                     speedLimit = GetEntitySpeed(vehicle)
                     SetEntityMaxSpeed(vehicle, speedLimit)
-                    TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Limitation de vitesse activée à " .. math.floor(speedLimit * 3.6) .. " km/h"}})
+                    TriggerEvent("chat:addMessage", {args = {Config.speedLimitEnabled .. math.floor(speedLimit * 3.6) .. " " .. speedUnit}})
                 else
                     SetEntityMaxSpeed(vehicle, 999.0)
-                    TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Limitation de vitesse désactivée"}})
+                    TriggerEvent("chat:addMessage", {args = {Config.speedLimitDisabled}})
                 end
             end
         end
@@ -33,16 +42,17 @@ RegisterCommand('limit', function(source, args)
         if (args[1] ~= nil) then
             local speed = tonumber(args[1])
             if (speed ~= nil) then
-                speedLimit = speed / 3.6
+                speedLimit = speed / speedRatio
                 SetEntityMaxSpeed(vehicle, speedLimit)
-                TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Limitation de vitesse activée à " .. speed .. " km/h"}})
+                TriggerEvent("chat:addMessage", {args = {Config.speedLimitEnabled .. speed .. " " .. speedUnit}})
+                limitSpeed = true
             else
-                TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Vitesse invalide"}})
+                TriggerEvent("chat:addMessage", {args = {Config.invalidSpeed}})
             end
         else
-            limitSpeed = false
             SetEntityMaxSpeed(vehicle, 999.0)
-            TriggerEvent("chat:addMessage", {args = {"^1[Limitateur de vitesse]^7 Limitation de vitesse désactivée"}})
+            TriggerEvent("chat:addMessage", {args = {Config.speedLimitDisabled}})
+            limitSpeed = false
         end
     end
 end)
